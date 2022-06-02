e1b1b8492abf84d75fe30105552d9f833130f982
Petter Karlsson <petter.karlsson@cmeducations.se>
2022-06-02 22:26:32 +0200

    formated

--- tms-penalty/resultat.php
+++ tms-penalty/resultat.php
@@ -13,24 +13,24 @@
 <?php
 $ipadress = $_SERVER['REMOTE_ADDR'];
 $today = date("Y-m-d H:i:s");
-if(!empty($_GET['namn'])){
+if (!empty($_GET['namn'])) {
 	$namn = $_GET['namn'];
 	$points = $_GET['points'];
 	$level = $_GET['level'];
 
 	$connection = mysql_connect("localhost", "", "") or die("Kunde inte skapa koppling!");
-	mysql_select_db("alvablot_com",$connection) or die("Kunde inte v�lja databas");
-	
+	mysql_select_db("alvablot_com", $connection) or die("Kunde inte v�lja databas");
+
 	$laggTill = "INSERT INTO spel(namn, poang, niva, datum, ip) VALUES ('$namn', '$points','$level','$today','$ipadress')";
 	mysql_query($laggTill) or die("Det gick inte att l�gga till information!");
 	header("Location:visa_resultat.php");
 	mysql_close($connection);
-
 }
 
 ?>
 
 <html>
+
 <head>
 	<title>Untitled</title>
 </head>
@@ -40,4 +40,5 @@ if(!empty($_GET['namn'])){
 
 
 </body>
+
 </html>
--- tms-penalty/visa_resultat.php
+++ tms-penalty/visa_resultat.php
@@ -1,5 +1,5 @@
 <?php
-header("Cache-Control: max-age=1, must-revalidate"); 
+header("Cache-Control: max-age=1, must-revalidate");
 ?>
 <html>
 <meta charset="UTF-8" />
@@ -14,57 +14,71 @@ header("Cache-Control: max-age=1, must-revalidate");
 //													//
 //////////////////////////////////////////////////////
 -->
+
 <head>
-<title>TMS Penalty Challenge</title>
-<LINK rel="StyleSheet" type="text/css" href="../css/default.css">
-<style>
-body,table,tr,td,div,span{
-	font:10px verdana;
-}
-a{font-weight:bold;}
-body{background:url(logga.jpg); background-repeat:no-repeat;}
-</style>
-<script>
-function tebax(){
-	window.location='index.html'
-}
-function avbryt(){
-	return false
-}
-document.oncontextmenu = avbryt
-</script>
+	<title>TMS Penalty Challenge</title>
+	<LINK rel="StyleSheet" type="text/css" href="../css/default.css">
+	<style>
+		body,
+		table,
+		tr,
+		td,
+		div,
+		span {
+			font: 10px verdana;
+		}
+
+		a {
+			font-weight: bold;
+		}
+
+		body {
+			background: url(logga.jpg);
+			background-repeat: no-repeat;
+		}
+	</style>
+	<script>
+		function tebax() {
+			window.location = 'index.html'
+		}
+
+		function avbryt() {
+			return false
+		}
+		document.oncontextmenu = avbryt
+	</script>
 </head>
 
 <body>
-<br><br><br><br><br><br><br><br>
-<?php
+	<br><br><br><br><br><br><br><br>
+	<?php
 	$connection = mysql_connect("localhost", "", "") or die("Kunde inte skapa koppling!");
-	mysql_select_db("alvablot_com",$connection) or die("Kunde inte v�lja databas");
-$hamta = "SELECT * FROM spel ORDER BY poang DESC";
-$resultat = mysql_query($hamta) or die("Det gick inte att h�mta information fr�n databasen!");
+	mysql_select_db("alvablot_com", $connection) or die("Kunde inte v�lja databas");
+	$hamta = "SELECT * FROM spel ORDER BY poang DESC";
+	$resultat = mysql_query($hamta) or die("Det gick inte att h�mta information fr�n databasen!");
 
-$i=1;
-print "<h3>TIO I TOPP</h3>";
-print '<table bgcolor=#999999 width=350 cellpadding=2 cellspacing=1><tr bgcolor=#cccccc><td><b class=blue>Rank</b></td><td><b class=blue>Namn</b></td><td><b class=blue>Po�ng</b></td><td><b class=blue>Niv�</b></td></tr>';
-while($rad = mysql_fetch_array($resultat)){
-	if($i<11) {
-	print '<tr bgcolor=#ffffff>';
-	print '<td>' . $i . "</td>";
-	print '<td>' . $rad["namn"] . "</td>";
-	print '<td>' . $rad["poang"] . "</td>";
-	print '<td>' . $rad["niva"] . "</td>";
-	print '</tr>';
+	$i = 1;
+	print "<h3>TIO I TOPP</h3>";
+	print '<table bgcolor=#999999 width=350 cellpadding=2 cellspacing=1><tr bgcolor=#cccccc><td><b class=blue>Rank</b></td><td><b class=blue>Namn</b></td><td><b class=blue>Po�ng</b></td><td><b class=blue>Niv�</b></td></tr>';
+	while ($rad = mysql_fetch_array($resultat)) {
+		if ($i < 11) {
+			print '<tr bgcolor=#ffffff>';
+			print '<td>' . $i . "</td>";
+			print '<td>' . $rad["namn"] . "</td>";
+			print '<td>' . $rad["poang"] . "</td>";
+			print '<td>' . $rad["niva"] . "</td>";
+			print '</tr>';
+		}
+		$i++;
 	}
-	$i++;
-
-}
-print '</table>';
+	print '</table>';
 	mysql_close($connection);
 
-print "<br>Spelet har spelats " . ($i).  " g�nger";
-?>
-<br><br>
-<a href="javascript:tebax()" style="border:1px solid #666666;padding:5px;">Spela!</a>
+	print "<br>Spelet har spelats " . ($i) .  " g�nger";
+	?>
+	<br><br>
+	<a href="javascript:tebax()" style="border:1px solid #666666;padding:5px;">Spela!</a>
 
 </body>
+
 </html>
